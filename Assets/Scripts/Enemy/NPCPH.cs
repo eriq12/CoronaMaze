8b042cdc8604538b8f9c237f3fe09d3f1e799f81
mrk2811 <mrk2811@users.noreply.github.com>
2021-02-27 17:11:29 -0500

    Added script
    added script under 'Enemy' folder

---
+++ Assets/Scripts/Enemy/NPCPH.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPH : MonoBehaviour
+{
+    public Transform playerPH;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = playerPH.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate() {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction){
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
