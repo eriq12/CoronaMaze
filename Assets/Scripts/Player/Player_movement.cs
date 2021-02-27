d3e8a2df60c0c32465ad97be5b1c0a15645344a2
Hydrosaber <hydrosaber11@gmail.com>
2021-02-26 23:10:19 -0500

    Scene and basic player movement
    Started code and prefabs for movement and walls.

---
+++ Assets/Scripts/Player/Player_movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.velocity = movement * speed;
+    }
+}
