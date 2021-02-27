1a051481d37b99e773a65bcf3f64236d6b8e7dbf
Hydrosaber <hydrosaber11@gmail.com>
2021-02-27 15:28:02 -0500

    Health and infection implemented
    Added health, health bar, and health depletion when near enemies (objects in enemy layer)

---
+++ Assets/Scripts/Player/Player_Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Health : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpBar;
+    private float currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = hpBar.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpBar.value = currentHP;
+    }
+
+    //takes health away
+    public void Damage(float hpDamage){
+        currentHP-=hpDamage;
+        if(currentHP < 0){
+            currentHP = 0;
+        }
+    }
+}
